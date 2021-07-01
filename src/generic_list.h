10eae07447065fe3e0671451ee8114a7bbe3c500
KonradFoit <konrad.foit@gmail.com>
2021-07-01 18:33:45 +0200

    Added MIT license text

--- src/generic_list.h
+++ src/generic_list.h
@@ -1,8 +1,34 @@
-/*
- * list.h
+/*********************************************************************************
+ * Copyright (c) 2021 Konrad Foit                                                *
+ *                                                                               *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  *
+ * of this software and associated documentation files (the "Software"), to deal *
+ * in the Software without restriction, including without limitation the rights  *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
+ * copies of the Software, and to permit persons to whom the Software is         *
+ * furnished to do so, subject to the following conditions:                      *
+ *                                                                               *
+ * The above copyright notice and this permission notice shall be included in all*
+ * copies or substantial portions of the Software.                               *
+ *                                                                               *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE *
+ * SOFTWARE.                                                                     *
+ *********************************************************************************/
+
+/** @file generic_list.c
+ * @author Konrad Foit
+ * @date 20.05.2021
+ * @brief This is a generic two-way list module with commonly used functions to
+ *        operate on the list
  *
- *  Created on: 20 maj 2021
- *      Author: konra
+ * module for easy handling of a two-way generic list with some commonly used
+ * functions to operate on such list
+ *
  */
 
 #ifndef SRC_TOOLS_GENERIC_LIST_H_
